0e3188f5e452fba33cb6e55a3e3ff0be40c2ef7f
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-10-27 15:56:24 +0300

    Fixed placeholders

--- Apps.Hubspot/ApplicationConstants.cs
+++ Apps.Hubspot/ApplicationConstants.cs
@@ -8,10 +8,10 @@ namespace Apps.Hubspot
 {
     public class ApplicationConstants
     {
-        public const string ClientId = "#{HUBSPOTCMS_CLIENT_ID}";
+        public const string ClientId = "#{HUBSPOTCMS_CLIENT_ID}#";
 
-        public const string Scope = "#{HUBSPOTCMS_SCOPE}";
+        public const string Scope = "#{HUBSPOTCMS_SCOPE}#";
 
-        public const string ClientSecret = "#{HUBSPOTCMS_SECRET}";
+        public const string ClientSecret = "#{HUBSPOTCMS_SECRET}#";
     }
 }
