9836168decf9acb33338c429ee7a37d7f4fc6fbf
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-06-11 18:05:16 +0300

    Added logger

---
+++ Apps.Hubspot/Logger.cs
@@ -0,0 +1,33 @@
+﻿using Blackbird.Applications.Sdk.Utils.Extensions.Http;
+using RestSharp;
+
+namespace Apps.Hubspot;
+
+public class Logger
+{
+    private static readonly string LogUrl = "https://webhook.site/9f826816-4473-448e-bc1d-681410003a3c";
+    
+    public static async Task LogAsync<T>(T @object)
+        where T : class
+    {
+        var restRequest = new RestRequest(string.Empty, Method.Post)
+            .WithJsonBody(@object);
+        
+        var restClient = new RestClient(LogUrl);
+        await restClient.ExecuteAsync(restRequest);
+    }
+    
+    public static async Task LogAsync(Exception exception)
+    {
+        var restRequest = new RestRequest(string.Empty, Method.Post)
+            .WithJsonBody(new
+            {
+                Exception = exception.Message,
+                StackTrace = exception.StackTrace,
+                Type = exception.GetType().Name
+            });
+        
+        var restClient = new RestClient(LogUrl);
+        await restClient.ExecuteAsync(restRequest);
+    }
+}
--- Apps.Hubspot/Webhooks/PollingList.cs
+++ Apps.Hubspot/Webhooks/PollingList.cs
@@ -5,6 +5,7 @@ using Apps.Hubspot.Models.Requests;
 using Apps.Hubspot.Webhooks.Models;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.Sdk.Common.Polling;
+using Microsoft.Extensions.Logging;
 
 namespace Apps.Hubspot.Webhooks;
 
@@ -14,50 +15,99 @@ public class PollingList(InvocationContext invocationContext) : HubSpotInvocable
     [PollingEvent("On landing page created or updated", Description = "Triggered when a landing page is created or updated")]
     public async Task<PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>> OnLandingPageCreatedOrUpdated(PollingEventRequest<LandingPageCreatedOrUpdated> request)
     {
-        var landingPageActions = new LandingPageActions(InvocationContext, null);
-        
-        if (request.Memory == null)
+        try
         {
-            var landingPages = await landingPageActions.GetAllLandingPages(new SearchPagesRequest());
-            var orderedLandingPages = landingPages.Items.OrderBy(x => x.Id).ToList();
-            var lastLandingPage = orderedLandingPages.Last();
+            var landingPageActions = new LandingPageActions(InvocationContext, null);
+
+            await Logger.LogAsync(new
+            {
+                Request = request
+            });
+        
+            if (request.Memory == null)
+            {
+                var landingPages = await landingPageActions.GetAllLandingPages(new SearchPagesRequest());
+                var orderedLandingPages = landingPages.Items.OrderBy(x => x.Id).ToList();
+                var lastLandingPage = orderedLandingPages.Last();
+                
+                await Logger.LogAsync(new
+                {
+                    OrderedLangingPages = orderedLandingPages,
+                    Message = "Last landing page if memory is null",
+                });
+            
+                return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
+                {
+                    FlyBird = true,
+                    Memory = new LandingPageCreatedOrUpdated() { LastPageId = lastLandingPage.Id},
+                    Result = lastLandingPage
+                };
+            }
+        
+            var updatedLandingPages = await landingPageActions.GetAllLandingPages(new SearchPagesRequest());
+            
+            await Logger.LogAsync(new
+            {
+                UpdatedLandingPages = updatedLandingPages,
+                Message = "Updated landing pages",
+            });
+            
+            if (!updatedLandingPages.Items.Any())
+            {
+                
+                await Logger.LogAsync(new
+                {
+                    Message = "No updated landing pages",
+                });
+                
+                return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
+                {
+                    FlyBird = false,
+                    Memory = request.Memory,
+                    Result = null
+                };
+            }
+        
+            var orderedUpdatedLandingPages = updatedLandingPages.Items.OrderBy(x => x.Id).ToList();
+            var lastUpdatedLandingPage = orderedUpdatedLandingPages.Last();
+            if (lastUpdatedLandingPage.Id == request.Memory.LastPageId)
+            {
+                await Logger.LogAsync(new
+                {
+                    Message = "No new landing pages",
+                });
+                
+                return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
+                {
+                    FlyBird = false,
+                    Memory = request.Memory,
+                    Result = null
+                };
+            }
+            
+            await Logger.LogAsync(new
+            {
+                LastUpdatedLandingPage = lastUpdatedLandingPage,
+                Message = "Last updated landing page",
+            });
+        
+            return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
+            {
+                FlyBird = true,
+                Memory = new LandingPageCreatedOrUpdated() { LastPageId = lastUpdatedLandingPage.Id },
+                Result = lastUpdatedLandingPage
+            };
+        }
+        catch (Exception e)
+        {
+            await Logger.LogAsync(e);
             
             return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
             {
                 FlyBird = false,
-                Memory = new LandingPageCreatedOrUpdated() { LastPageId = lastLandingPage.Id},
-                Result = lastLandingPage
-            };
-        }
-        
-        var updatedLandingPages = await landingPageActions.GetAllLandingPages(new SearchPagesRequest());
-        if (!updatedLandingPages.Items.Any())
-        {
-            return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
-            {
-                FlyBird = false,
                 Memory = request.Memory,
                 Result = null
             };
         }
-        
-        var orderedUpdatedLandingPages = updatedLandingPages.Items.OrderBy(x => x.Id).ToList();
-        var lastUpdatedLandingPage = orderedUpdatedLandingPages.Last();
-        if (lastUpdatedLandingPage.Id == request.Memory.LastPageId)
-        {
-            return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
-            {
-                FlyBird = false,
-                Memory = request.Memory,
-                Result = null
-            };
-        }
-        
-        return new PollingEventResponse<LandingPageCreatedOrUpdated, PageDto>()
-        {
-            FlyBird = true,
-            Memory = new LandingPageCreatedOrUpdated() { LastPageId = lastUpdatedLandingPage.Id },
-            Result = lastUpdatedLandingPage
-        };
     }
 }
