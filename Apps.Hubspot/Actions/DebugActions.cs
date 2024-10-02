e036e673aee4abad17bbafcbd8f0c08d810c5759
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-02 17:25:34 +0300

    Added debug action

---
+++ Apps.Hubspot/Actions/DebugActions.cs
@@ -0,0 +1,17 @@
+﻿using Apps.Hubspot.Actions.Base;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
+
+namespace Apps.Hubspot.Actions;
+
+[ActionList]
+public class DebugActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient) : BasePageActions(invocationContext, fileManagementClient)
+{
+    [Action("[DEBUG] Get credential providers", Description = "Get all credential providers. Can be used only in development environment.")]
+    public List<AuthenticationCredentialsProvider> GetCredentialProviders()
+    {
+        return InvocationContext.AuthenticationCredentialsProviders.ToList();
+    }
+}
