dcdcdb10f8e762da7e4ad719831a1f0c9896c122
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-10-03 15:56:50 +0300

    Added data source

--- Apps.Hubspot/Models/Requests/Forms/MarketingFormRequest.cs
+++ Apps.Hubspot/Models/Requests/Forms/MarketingFormRequest.cs
@@ -1,10 +1,12 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Hubspot.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Hubspot.Models.Requests.Forms;
 
 public class MarketingFormRequest
 {
-    [Display("Form ID", Description = "The ID of the form to retrieve")]
+    [Display("Form ID", Description = "The ID of the form to retrieve"), DataSource(typeof(MarketingFormDataHandler))]
     public string FormId { get; set; } = default!;
 
     public override string ToString()
