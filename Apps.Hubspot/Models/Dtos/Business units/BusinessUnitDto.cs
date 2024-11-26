d283acbda2740e824acd825e43de67ddf90dbf96
Artem Riabushenko <artemryabushenko@gmail.com>
2024-11-26 22:27:07 +0200

    Changes 19

--- Apps.Hubspot/Models/Dtos/Business units/BusinessUnitDto.cs
+++ Apps.Hubspot/Models/Dtos/Business units/BusinessUnitDto.cs
@@ -1,4 +1,5 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
@@ -11,11 +12,13 @@ namespace Apps.Hubspot.Models.Dtos.Business_units
     public class BusinessUnitDto
     {
         [Display("Business unit ID")]
+        [JsonProperty("id")]
         public string BusinessUnitId {  get; set; }
         [Display("Names of business units", Description= "The names of Business Units to retrieve. If empty or not provided, then all associated Business Units will be returned.")]
+        [JsonProperty("name")]
         public string Name { get; set; }
-        [Display("Logo meta data")]
-        public LogoMetadata LogoMetadata { get; set; }
+        //[Display("Logo meta data")]
+        //public LogoMetadata LogoMetadata { get; set; }
 
     }
 
