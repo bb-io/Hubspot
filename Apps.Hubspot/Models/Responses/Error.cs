7b2a7f94c81a042c4844a746ecb9188aad34f684
Artem Riabushenko <artemryabushenko@gmail.com>
2024-11-26 22:48:20 +0200

    Changes 21

--- Apps.Hubspot/Models/Responses/Error.cs
+++ Apps.Hubspot/Models/Responses/Error.cs
@@ -1,7 +1,12 @@
-﻿namespace Apps.Hubspot.Models.Responses;
+﻿using Newtonsoft.Json;
+
+namespace Apps.Hubspot.Models.Responses;
 
 public class Error
 {
+    [JsonProperty("status")]
     public string ErrorType { get; set; }
+
+    [JsonProperty("message")]
     public string Message { get; set; }
 }
