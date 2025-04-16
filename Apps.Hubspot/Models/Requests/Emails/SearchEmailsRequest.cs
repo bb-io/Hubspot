dca8895d1955ea68ddc7193fb1850e8159d1bb02
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-04-16 22:34:40 +0200

    Add email creation date properties and update method signature.

--- Apps.Hubspot/Models/Requests/Emails/SearchEmailsRequest.cs
+++ Apps.Hubspot/Models/Requests/Emails/SearchEmailsRequest.cs
@@ -1,4 +1,5 @@
 using Apps.Hubspot.DataSourceHandlers;
+using Apps.Hubspot.Utils.Converters;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Newtonsoft.Json;
@@ -15,4 +16,19 @@ public class SearchEmailsRequest : TimeFilterRequest
     [Display("Language")]
     [JsonProperty("language")]
     public string? Language { get; set; }
+
+    [Display("Created at")]
+    [JsonProperty("createdAt__eq")]
+    [JsonConverter(typeof(DateTimeToUnixEpoch))]
+    public DateTime? CreatedAt { get; set; }//TODO: remove, WIP
+
+    [Display("Created after")]
+    [JsonProperty("createdAt__gt")]
+    [JsonConverter(typeof(DateTimeToUnixEpoch))]
+    public DateTime? CreatedAfter { get; set; }//TODO: remove, WIP
+
+    [Display("Created before")]
+    [JsonProperty("createdAt__lt")]
+    [JsonConverter(typeof(DateTimeToUnixEpoch))]
+    public DateTime? CreatedBefore { get; set; } //TODO: remove, WIP
 }
