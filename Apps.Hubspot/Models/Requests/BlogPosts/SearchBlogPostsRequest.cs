05900aad579905d3e0c8e9dd143603ee9bad4fd2
ce-nistal <cecilia.nistal@blackbird.io>
2025-07-01 17:51:03 -0300

    Updated search states for blog posts

---
+++ Apps.Hubspot/Models/Requests/BlogPosts/SearchBlogPostsRequest.cs
@@ -0,0 +1,48 @@
+using Apps.Hubspot.DataSourceHandlers;
+using Apps.Hubspot.DataSourceHandlers.Static;
+using Apps.Hubspot.Utils.Converters;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+using Newtonsoft.Json;
+
+namespace Apps.Hubspot.Models.Requests;
+
+public class SearchBlogPostsRequest : PagesTimeFilterRequest
+{
+    [Display("Not translated in language"), StaticDataSource(typeof(LanguageHandler))]
+    [JsonIgnore]
+    public string? NotTranslatedInLanguage { get; set; }
+
+    [Display("Language", Description = "Does not support language locale values (e.g., en-us)"), StaticDataSource(typeof(LanguageHandler))]
+    [JsonProperty("language__in")]
+    public string? Language { get; set; }
+
+    [Display("Site name contains")]
+    [JsonProperty("name__icontains")]
+    public string? NameContains { get; set; }
+
+    [Display("Site name equals")]
+    [JsonProperty("name__eq")]
+    public string? NameEquals { get; set; }
+
+    [Display("Slug")]
+    [JsonProperty("slug__eq")]
+    public string? Slug { get; set; }
+
+    [Display("State")]
+    [JsonProperty("state__eq"), StaticDataSource(typeof(BlogPostStateHandler))]
+    public string? State { get; set; }
+
+    [Display("Domain equals"), DataSource(typeof(DomainDataHandler))]
+    [JsonProperty("domain__eq")]
+    public string? PageDomain { get; set; }
+
+    [Display("Domain contains"), DataSource(typeof(DomainDataHandler))]
+    [JsonProperty("domain__contains")]
+    public string? PageDomainContains { get; set; }
+
+    [Display("Url contains")]
+    [JsonIgnore]
+    public string? UrlContains { get; set; }
+}
