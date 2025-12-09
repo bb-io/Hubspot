ac38dc9411810c1b2eb6e208ce866e418d22b256
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2025-12-09 16:55:45 +0200

    Added 'Get account info' debug action

--- Apps.Hubspot/Actions/DebugActions.cs
+++ Apps.Hubspot/Actions/DebugActions.cs
@@ -1,4 +1,7 @@
-﻿using Blackbird.Applications.Sdk.Common;
+﻿using Apps.Hubspot.Api;
+using Apps.Hubspot.Invocables;
+using Apps.Hubspot.Models.Responses;
+using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Invocation;
@@ -6,11 +9,18 @@ using Blackbird.Applications.Sdk.Common.Invocation;
 namespace Apps.Hubspot.Actions;
 
 [ActionList] 
-public class DebugActions(InvocationContext invocationContext) : BaseInvocable(invocationContext) 
+public class DebugActions(InvocationContext invocationContext) : HubSpotInvocable(invocationContext) 
 {
     [Action("Debug", Description = "Debug action")]
     public List<AuthenticationCredentialsProvider> DebugAction()
     {
         return InvocationContext.AuthenticationCredentialsProviders.ToList();
+    }
+    
+    [Action("Get account info", Description = "Retrieves account information for the connected Hubspot account. Useful only for debugging purposes.")]
+    public async Task<AccountInfoResponse> GetAccountInformation()
+    {
+        var response = await GetAccountInfo();
+        return response;
     }
 }
